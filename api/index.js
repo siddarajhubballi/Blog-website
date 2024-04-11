3dec74fe9a3bd7d5824fe331c6ae0cbd1106f50f
siddarajhubballi <siddarajshubballi@gmail.com>
2024-04-11 08:36:40 +0530

    mongodb connection

--- api/index.js
+++ api/index.js
@@ -1,6 +1,19 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
+
+mongoose
+  .connect(process.env.MONGODB)
+  .then(() => {
+    console.log("Connected to Mongodb");
+  })
+  .catch((err) => {
+    console.log(`Error while connecting to Mongodb :: ${err}`);
+  });
 
 app.listen(4000, () => {
   console.log("Server listening at 4000");
