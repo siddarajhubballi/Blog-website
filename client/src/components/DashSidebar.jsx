5839baed85fc3b563a591d77f42ac65f464e3cc4
siddarajhubballi <siddarajshubballi@gmail.com>
2024-04-21 19:34:52 +0530

    complete sidebar of dashboard

---
+++ client/src/components/DashSidebar.jsx
@@ -0,0 +1,35 @@
+import { Sidebar } from 'flowbite-react'
+import React from 'react'
+import { HiArrowSmRight, HiUser } from "react-icons/hi"
+import { useState, useEffect } from 'react';
+import { useLocation, Link } from 'react-router-dom';
+
+export default function DashSidebar() {
+    const location = useLocation();
+
+    const [tab, setTab] = useState("");
+
+    useEffect(() => {
+        const urlParams = new URLSearchParams(location.search);
+        const tab = urlParams.get("tab");
+        if(tab) {
+        setTab(tab);
+        }
+    }, [location.search]);
+    return (
+        <Sidebar className="w-full md:w-56">
+            <Sidebar.Items>
+                <Sidebar.ItemGroup>
+                    <Link to="/dashboard?tab=profile">
+                        <Sidebar.Item active={tab === "profile"} icon={HiUser} label={"User"}>
+                            Profile
+                        </Sidebar.Item>
+                    </Link>
+                    <Sidebar.Item icon={HiArrowSmRight} >
+                        Sign out
+                    </Sidebar.Item>
+                </Sidebar.ItemGroup>
+            </Sidebar.Items>
+        </Sidebar>
+    )
+}
--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,9 +1,32 @@
 import React from "react";
+import { useLocation } from "react-router-dom";
+import { useState, useEffect } from "react";
+import DashSidebar from "../components/DashSidebar";
+import DashProfile from "../components/DashProfile";
 
 export default function Dashboard() {
+  const location = useLocation();
+
+  const [tab, setTab] = useState("");
+
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const tab = urlParams.get("tab");
+    if(tab) {
+      setTab(tab);
+    }
+  }, [location.search]);
+
   return (
-    <div>
-      <h5>dashboard</h5>
+    <div className="min-h-screen flex flex-col md:flex-row">
+      <div className="md:w-56">
+        {/* Sidebar */}
+        <DashSidebar />
+      </div>
+      <div>
+        {/* Main */}
+        {tab === "profile" && <DashProfile />}
+      </div>
     </div>
   );
 }
